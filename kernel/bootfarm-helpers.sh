472ae24b94a3faf4deb4785c018a922cfa0559b2
Heiko Stuebner <heiko@sntech.de>
2018-06-01 21:43:37 +0200

    move build_initramfs code to bootfarm-helpers

--- kernel/bootfarm-helpers.sh
+++ kernel/bootfarm-helpers.sh
@@ -333,6 +333,41 @@ setup_imagedata() {
 	tar -C _bootfarm/images/$ARCH/dtbs -xzf _bootfarm/$ARCH/dtbs-$ARCH.tar.gz
 }
 
+build_initramfs() {
+	local ARCH=$1
+	local INST=$2
+	local BUILDPLACE=/home/devel/nfs/rootfs-$INST
+
+	if [ ! -d $BUILDPLACE ]; then
+		echo "$INST: instance not found"
+		exit 1
+	fi
+
+	if [ ! -f /home/devel/nfs/kernel/$ARCH/kernel.release ]; then
+		echo "$ARCH: kernel.release missing"
+		exit 1
+	fi
+
+	local KVER=`cat /home/devel/nfs/kernel/$ARCH/kernel.release`
+	local CHROOTEXEC="/usr/sbin/chroot $BUILDPLACE "
+
+	echo "$INST: creating initramfs for $KVER"
+	set +e
+	$CHROOTEXEC update-initramfs -d -k $KVER >/dev/null 2>&1
+	set -e
+	$CHROOTEXEC update-initramfs -c -k $KVER >/dev/null
+
+	if [ ! -f $BUILDPLACE/boot/initrd.img-$KVER ]; then
+		echo "$INST: initramfs creation failed"
+		exit 1
+	fi
+
+	if [ -f $BUILDPLACE/boot/initrd.img ]; then
+		rm $BUILDPLACE/boot/initrd.img
+	fi
+	mv $BUILDPLACE/boot/initrd.img-$KVER $BUILDPLACE/boot/initrd.img
+}
+
 #
 # Generates a coreboot partition image for ChromeOS devices.
 #
--- server/rebuild-initramfs.sh
+++ server/rebuild-initramfs.sh
@@ -1,38 +1,5 @@
 #!/bin/bash -e
 
-build_initramfs() {
-	local ARCH=$1
-	local INST=$2
-	local BUILDPLACE=/home/devel/nfs/rootfs-$INST
-
-	if [ ! -d $BUILDPLACE ]; then
-		echo "$INST: instance not found"
-		exit 1
-	fi
-
-	if [ ! -f /home/devel/nfs/kernel/$ARCH/kernel.release ]; then
-		echo "$ARCH: kernel.release missing"
-		exit 1
-	fi
-
-	local KVER=`cat /home/devel/nfs/kernel/$ARCH/kernel.release`
-	local CHROOTEXEC="/usr/sbin/chroot $BUILDPLACE "
-
-	echo "$INST: creating initramfs for $KVER"
-	set +e
-	$CHROOTEXEC update-initramfs -d -k $KVER >/dev/null 2>&1
-	set -e
-	$CHROOTEXEC update-initramfs -c -k $KVER >/dev/null
-
-	if [ ! -f $BUILDPLACE/boot/initrd.img-$KVER ]; then
-		echo "$INST: initramfs creation failed"
-		exit 1
-	fi
-
-	if [ -f $BUILDPLACE/boot/initrd.img ]; then
-		rm $BUILDPLACE/boot/initrd.img
-	fi
-	mv $BUILDPLACE/boot/initrd.img-$KVER $BUILDPLACE/boot/initrd.img
-}
+. /home/devel/hstuebner/bootfarm/server/bootfarm-helpers.sh
 
 build_initramfs $1 $2
