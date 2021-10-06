mmind/boardfarm-scripts

commit 0556f5afe015630e8813ab7bf37516231a3effe8

generalize module handling so that it can be reused for localimages

server/rebuild-netboot.sh
@@ -41,20 +41,6 @@ extract_dtbs() {
 	tar -C /home/devel/nfs/kernel/$ARCH/dtbs -xzf /home/devel/nfs/kernel/$ARCH/dtbs-$ARCH.tar.gz
 }
 
-unpack_modules() {
-	local ARCH=$1
-	local INST=$2
-	local TARGET=$3
-
-	if [ -d /home/devel/nfs/kernel/$INST ]; then
-		echo "$INST: unpacking special modules"
-		tar -C $TARGET/lib/modules -xzf /home/devel/nfs/kernel/$INST/modules-$INST.tar.gz
-	else
-		echo "$INST: unpacking $ARCH modules"
-		tar -C $TARGET/lib/modules -xzf /home/devel/nfs/kernel/$ARCH/modules-$ARCH.tar.gz
-	fi
-}
-
 if [ "$#" = "1" ]; then
 	TARGET=$1
 else
@@ -79,15 +65,7 @@ for i in `cat /home/devel/nfs/instances | grep -v "^#"`; do
 		continue
 	fi
 
-	if [ -d /home/devel/nfs/rootfs-$INST/lib/modules ]; then
-		set +e
-		rm -rf /home/devel/nfs/rootfs-$INST/lib/modules/*
-		set -e
-	else
-		sudo mkdir /home/devel/nfs/rootfs-$INST/lib/modules
-		sudo chown hstuebner.hstuebner /home/devel/nfs/rootfs-$INST/lib/modules
-	fi
-
+	setup_modules $ARCH $INST /home/devel/nfs/rootfs-$INST
 	unpack_modules $ARCH $INST /home/devel/nfs/rootfs-$INST
 
 	# only build initramfs for arm64 for now

kernel/bootfarm-helpers.sh
@@ -324,6 +324,35 @@ trigger_bootfarm() {
 	fi
 }
 
+setup_modules() {
+	local ARCH=$1
+	local INST=$2
+	local TARGET=$3
+
+	if [ -d $TARGET/lib/modules ]; then
+		set +e
+		rm -rf $TARGET/lib/modules/*
+		set -e
+	else
+		sudo mkdir $TARGET/lib/modules
+		sudo chown hstuebner.hstuebner $TARGET/lib/modules
+	fi
+}
+
+unpack_modules() {
+	local ARCH=$1
+	local INST=$2
+	local TARGET=$3
+
+	if [ -d /home/devel/nfs/kernel/$INST ]; then
+		echo "$INST: unpacking special modules"
+		tar -C $TARGET/lib/modules -xzf /home/devel/nfs/kernel/$INST/modules-$INST.tar.gz
+	else
+		echo "$INST: unpacking $ARCH modules"
+		tar -C $TARGET/lib/modules -xzf /home/devel/nfs/kernel/$ARCH/modules-$ARCH.tar.gz
+	fi
+}
+
 #
 # Setup data for the local image generation.
 # This includes getting the kernel image from the build directory

commit 4f0071853cc8034c73b992e02a400fd32751db36

backend: actually include boardfarm-helpers in rebuild-netboot
The commit that moved the module handling to a more general location
forgot to include the necessary file in rebuild-netboot.

Looks like the boardfarm backend wasn't updated in a while, but
this broke creating netboot images after an update today.

Add the necessary include now.

Fixes: 0556f5afe015 ("generalize module handling so that it can be reused for localimages")
Signed-off-by: Heiko Stuebner <heiko@sntech.de>

server/rebuild-netboot.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
+. /home/devel/hstuebner/bootfarm/server/bootfarm-helpers.sh
+
 # FIXME: move to FIT image
 build_uimage() {
 	local ARCH=arm32