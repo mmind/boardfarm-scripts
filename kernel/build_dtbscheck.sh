833a4a2a1f02f82afc9089d4bf98e83c4eaaf3cc
Heiko Stuebner <heiko@sntech.de>
2022-07-06 17:58:03 +0200

    maintainer-scripts: add some more riscv{32,64} cases to general scripts
    Handle the riscv stuff also in dtbs, dtbscheck and full builds.
    
    Signed-off-by: Heiko Stuebner <heiko@sntech.de>

--- kernel/build_dtbscheck.sh
+++ kernel/build_dtbscheck.sh
@@ -8,7 +8,7 @@ else
 	target=$1
 fi
 
-for arch in arm32 arm64; do
+for arch in arm32 arm64 riscv32 riscv64; do
 	if [ "$target" != "all" ] && [ "$arch" != "$target" ]; then
 		echo "skipping $arch, not target $target"
 		continue
--- kernel/build_full.sh
+++ kernel/build_full.sh
@@ -29,14 +29,17 @@ elif [ -d core/tee ]; then
 else
 	build_kernel arm32
 	build_kernel arm64
+	build_kernel riscv32
 	build_kernel riscv64
 
 	install_kernel arm32
 	install_kernel arm64
+	install_kernel riscv32
 	install_kernel riscv64
 
 	install_dtbs arm32 "rk"
 	install_dtbs arm64 "rk px30"
+	install_dtbs riscv32 "jh7100 sun20i-d1 microchip-mpfs-icicle"
 	install_dtbs riscv64 "jh7100 sun20i-d1 microchip-mpfs-icicle"
 
 	trigger_bootfarm
